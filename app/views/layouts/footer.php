346f9c9fa864d9fba02ceea89188b4c7390aad9a
pablodelriob <pablodelriob@gmail.com>
2025-06-22 10:57:35 -0300

    Delete app directory

--- app/views/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-    <footer class="bg-dark text-white text-center py-3 mt-5">
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> Sistema de Inventario El Fénix. Todos los derechos reservados.</p>
-        </div>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-</html>
